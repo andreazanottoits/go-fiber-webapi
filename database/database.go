d3961d101d65c7fbbb743b16873e3fa7db80591b
andreazanottoits <azanotto80@gmail.com>
2022-08-05 14:04:28 +0200

    Add connect to db function

--- database/database.go
+++ database/database.go
@@ -14,3 +14,19 @@ type DbInstance struct {
 }
 
 var Database DbInstance
+
+func ConnectDb() {
+	db, err := gorm.Open(sqlite.Open("sqliteDatabase.db"), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to the database! \n", err)
+		os.Exit(2)
+	}
+
+	log.Println("Connected Successfully to Database")
+	db.Logger = logger.Default.LogMode(logger.Info)
+
+	Database = DbInstance{
+		Db: db,
+	}
+}
