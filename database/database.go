37879cb15bd4d84c21a3df27cf03fa74f4ad0ba1
andreazanottoits <azanotto80@gmail.com>
2022-08-05 14:04:09 +0200

    Add dependencies and database instance

--- database/database.go
+++ database/database.go
@@ -1,1 +1,16 @@
 package database
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type DbInstance struct {
+	Db *gorm.DB
+}
+
+var Database DbInstance
