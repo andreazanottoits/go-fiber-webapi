0de1e0117347472e91ad6338903f06370743e796
andreazanottoits <azanotto80@gmail.com>
2022-08-05 16:25:59 +0200

    Add database connection call

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"log"
+	"web-api/database"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	database.ConnectDb()
     app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
