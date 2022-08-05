8de09ce6af48199f78d7de7b171a018cee458927
andreazanottoits <azanotto80@gmail.com>
2022-08-05 18:10:46 +0200

    Add routes registration

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"web-api/database"
+	"web-api/routes"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -14,6 +15,15 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
+
+	app.Post("/api/user", routes.CreateUser)
+	app.Get("/api/users", routes.GetUsers)
+	app.Get("/api/user/:id", routes.GetUser)
+	app.Delete("/api/user/:id", routes.DeleteUser)
+
+	app.Post("/api/car", routes.CreateCar)
+	app.Get("/api/cars", routes.GetCars)
+	app.Delete("/api/users/:id", routes.DeleteCar)
 
 	log.Fatal(app.Listen(":3000"))
 }
